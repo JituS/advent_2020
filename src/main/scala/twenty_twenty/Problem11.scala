f6c3d90a18c952f7d2317fcdf1773d05f07568cf
Jitendra <Jitendrs@thoughtworks.com>
2020-12-12 03:06:12 +0530

    problem 11 2nd part

--- src/main/scala/twenty_twenty/Problem11.scala
+++ src/main/scala/twenty_twenty/Problem11.scala
@@ -6,34 +6,33 @@ object Problem11 {
   def main(args: Array[String]): Unit = {
     val list: Seq[(List[(String, Int)], Int)] = Source.fromInputStream(getClass.getResourceAsStream("problem11")).getLines()
       .map(e => e.split("").zipWithIndex.toList).zipWithIndex.toList
-
-    val value = shuffleSeats(list)
-    println(value.flatMap(_._1.flatMap(_._1)).count(_ == '#'))
+    println(shuffleSeats(list).flatMap(_._1.flatMap(_._1)).count(_ == '#'))
   }
 
-  def getAdjacent(original: Seq[(List[(String, Int)], Int)], row: Int, col: Int): Seq[String] = {
-    val (minX, maxX) = (if (row == 0) 0 else row - 1, if (row == original.length - 1) row else row + 1)
-    val (minY, maxY) = (if (col == 0) 0 else col - 1, if (col == original.head._1.length - 1) col else col + 1)
-    (minX to maxX).flatMap { x =>
-      (minY to maxY).map { y =>
-        if (x != row || y != col) Some(original(x)._1(y)._1)
-        else None
-      }
-    }.filter(_.isDefined).map(_.get).toList
+  def getAdjacents(arrangement: Seq[(List[(String, Int)], Int)], row: Int, col: Int): Seq[String] = {
+    val rowBackward = row - 1 to 0 by -1
+    val rowForward = row + 1 until arrangement.length
+    val columnForward = col + 1 until arrangement.head._1.length
+    val columnBackward = col - 1 to 0 by -1
+    List(rowForward.map((_, col)), rowBackward.map((_, col)), columnForward.map((row, _)), columnBackward.map((row, _)),
+      rowForward.zip(columnForward), rowForward.zip(columnBackward), rowBackward.zip(columnForward), rowBackward.zip(columnBackward)
+    )
+      .flatMap(_.find(seatIndex => Seq("L", "#").contains(arrangement(seatIndex._1)._1(seatIndex._2)._1)).map(e => arrangement(e._1)._1(e._2)._1))
+      .filter(_.nonEmpty)
   }
 
   @scala.annotation.tailrec
-  def shuffleSeats(original: Seq[(List[(String, Int)], Int)]): Seq[(List[(String, Int)], Int)] = {
-    val result = original.map { row =>
-      val newSymbol = row._1.map { col: (String, Int) =>
-        val adjacent = getAdjacent(original, row._2, col._2)
-        if (col._1 == "L" && adjacent.count(_ == "#") == 0) ("#", col._2)
-        else if (col._1 == "#" && adjacent.count(_ == "#") >= 4) ("L", col._2)
-        else col
+  def shuffleSeats(arrangement: Seq[(List[(String, Int)], Int)]): Seq[(List[(String, Int)], Int)] = {
+    val newArrangement = arrangement.map { row =>
+      val seatStatus = row._1.map { seat: (String, Int) =>
+        val adjacent = getAdjacents(arrangement, row._2, seat._2)
+        if (seat._1 == "L" && adjacent.count(_ == "#") == 0) ("#", seat._2)
+        else if (seat._1 == "#" && adjacent.count(_ == "#") >= 5) ("L", seat._2)
+        else seat
       }
-      (newSymbol, row._2)
+      (seatStatus, row._2)
     }
-    if (!original.equals(result)) shuffleSeats(result)
-    else result
+    if (!arrangement.equals(newArrangement)) shuffleSeats(newArrangement)
+    else newArrangement
   }
 }
