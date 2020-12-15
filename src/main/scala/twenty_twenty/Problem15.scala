119a9ce115960fc3d04693759ec616be3ae027f9
Jitendra <Jitendrs@thoughtworks.com>
2020-12-16 03:35:10 +0530

    problem 15 part 2

--- src/main/scala/twenty_twenty/Problem15.scala
+++ src/main/scala/twenty_twenty/Problem15.scala
@@ -4,19 +4,12 @@ import scala.collection.mutable
 
 object Problem15 {
   def main(args: Array[String]): Unit = {
-    val input = mutable.LinkedHashMap(0 -> List(1), 13 -> List(2), 1 -> List(3), 16 -> List(4), 6 -> List(5), 17 -> List(6))
-    (7 to 2020).foldLeft(input) { (result, turn) =>
-      val last: (Int, List[Int]) = result.last
-      val nextNumber = if (last._2.length == 1) 0 else last._2.last - last._2(last._2.length - 2)
-      if (result.contains(nextNumber)) {
-        val option: List[Int] = result.remove(nextNumber).get
-        result.update(nextNumber, option :+ turn)
-        result
-      } else {
-        result.update(nextNumber, List(turn))
-        result
-      }
+    val numberWithSpokenTurn = mutable.LinkedHashMap(0 -> List(1), 13 -> List(2), 1 -> List(3), 16 -> List(4), 6 -> List(5), 17 -> List(6))
+    (7 to 30000000).foreach { turn =>
+      val lastSpoken = numberWithSpokenTurn.last._2
+      val currentSpoken = if (lastSpoken.length == 1) 0 else lastSpoken.last - lastSpoken(lastSpoken.length - 2)
+      numberWithSpokenTurn.update(currentSpoken, (numberWithSpokenTurn.remove(currentSpoken).getOrElse(List()) :+ turn).takeRight(2))
     }
-    println(input.last)
+    println(numberWithSpokenTurn.last)
   }
 }
